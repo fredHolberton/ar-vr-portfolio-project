fd4728e581a814222e5175ec0d7aa68417a485a4
fredHolberton <10196@holbertonstudents.com>
2025-08-05 10:04:46 +0200

    AR/VR - Portfolio Project - 0

---
+++ Assets/Scripts/ChessBoadController.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChessBoadController : MonoBehaviour
+{
+    [SerializeField] private GameObject bishopLight = null;
+    [SerializeField] private GameObject knightLight = null;
+    [SerializeField] private GameObject pawnDark = null;
+    [SerializeField] private GameObject rookDark = null;
+    [SerializeField] private GameObject[] chessPieces = null;
+    [SerializeField] private Sprite bishopLightImage = null;
+    [SerializeField] private Sprite knightLightImage = null;
+    [SerializeField] private Sprite pawnDarkImage = null;
+    [SerializeField] private Sprite rookDarkImage = null;
+
+    [SerializeField] private TextMeshProUGUI nbFoundPiecesText = null;
+    private int speed = 45;
+
+    private int nbFoundPieces;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nbFoundPiecesText.text = "0";
+        nbFoundPieces = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            chessPieces[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TurnAround()
+    {
+        transform.Rotate(Vector3.up * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "BishopLight" || other.gameObject.name == "KnightLight" || other.gameObject.name == "PawnDark" || other.gameObject.name == "RookDark")
+        {
+
+            if (other.gameObject.name == "BishopLight")
+            {
+                other.gameObject.SetActive(false);
+                bishopLight.SetActive(true);
+                chessPieces[nbFoundPieces].GetComponent<Image>().sprite = bishopLightImage;
+            }
+            else if (other.gameObject.name == "KnightLight")
+            {
+                other.gameObject.SetActive(false);
+                knightLight.SetActive(true);
+
+            }
+            else if (other.gameObject.name == "PawnDark")
+            {
+                other.gameObject.SetActive(false);
+                pawnDark.SetActive(true);
+
+            }
+            else if (other.gameObject.name == "RookDark")
+            {
+                other.gameObject.SetActive(false);
+                rookDark.SetActive(true);
+
+            }
+            chessPieces[nbFoundPieces].SetActive(true);
+            nbFoundPieces += 1;
+        }
+        
+
+        nbFoundPiecesText.text = string.Format("{0}", nbFoundPieces);
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# This repository contains files of project: Escape Station.
+![image](https://github.com/user-attachments/assets/731492c1-e25b-4d04-9ce0-678ff0deba0b)
+
+## This AR/VR project is intended to validate my training year at Holberton School
+Special thanks to the authors of the sound and visual resources that allowed me to add some relatable elements to this game:
+- Sound of oars on the lake: https://lasonotheque.org/ramer-lentement-s1514.html
+- Boat object: https://assetstore.unity.com/packages/3d/vehicles/sea/old-rowboat-31917
+- Station: https://github.com/hs-hq/spe_arvr_resources/tree/main/Unity%20-%20VR%20Room/Assets
+
+About myself:
+- Frédéric Urbani
+- LikedIn : https://www.linkedin.com/in/fr%C3%A9d%C3%A9ric-urbani-16211759/
+
